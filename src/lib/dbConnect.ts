25f58c4eadf71e9a875beb1581592867429fdd79
Pranav Molawade <pranavmolawade@Pranavs-MacBook-Air.local>
2024-06-29 14:55:31 +0530

    DB changed

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,23 +1,41 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions, Mongoose } from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-export const connect = async () => {
-    const connectionState = mongoose.connection.readyState;
-    if (connectionState === 1) {
-        console.log("Connected")
-        return;
-    }
-    if(connectionState === 2){
-        console.log("Connecting")
-        return;
-    }
-    try {
-         mongoose.connect(MONGODB_URI!,{
-            dbName: "RestAPI_Note",
-            bufferCommands:false
-         });
-    } catch (error) {
-        console.log("Error connecting")
-    }
+const MONGODB_URL = process.env.MONGODB_URI;
+
+if (!MONGODB_URL) {
+    throw new Error(
+        "Please define the MONGODB_URL environment variable inside .env.local"
+    );
 }
-export default connect;
+
+let cached: { conn: Mongoose | null; promise: Promise<Mongoose> | null } = {
+    conn: null,
+    promise: null,
+};
+
+const dbConnect = async (): Promise<Mongoose> => {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts: ConnectOptions = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
+    return cached.conn;
+};
+
+export default dbConnect;
